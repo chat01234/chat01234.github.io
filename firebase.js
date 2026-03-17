0b6d060fb887c788e0e393540ada9f7c878aefb2
chat01234 <pipepavi@gmail.com>
2026-03-17 18:35:14 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,11 @@
 
-// IMPORTS FIREBASE (VERSIÓN MODULAR)
+// IMPORTS
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
-// CONFIGURACIÓN DE FIREBASE
+// CONFIG REAL (TU PROYECTO)
 const firebaseConfig = {
   apiKey: "AIzaSyB8MX9UKhyhatywGhPbpIldBD80wsY38BI",
   authDomain: "chat01234.firebaseapp.com",
@@ -14,10 +15,11 @@ const firebaseConfig = {
   appId: "1:861947372030:web:3f1d0c4709b3d96fe6a0af"
 };
 
-// INICIALIZAR FIREBASE
+// INIT
 const app = initializeApp(firebaseConfig);
 
-// EXPORTAR SERVICIOS
+// EXPORTS
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+export const auth = getAuth(app);
 
